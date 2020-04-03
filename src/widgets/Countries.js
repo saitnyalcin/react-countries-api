4a3a08fdc7e661bd25b0e2514b4c49757d40b398
steveyalcin <steven.yalcin@gmail.com>
2020-04-02 21:42:42 -0300

    All countries will be showing in the UI to be ready for making a search

--- src/widgets/Countries.js
+++ src/widgets/Countries.js
@@ -3,15 +3,13 @@ import React, { useEffect, useState } from 'react';
 
 function Countries() {
   const [data, setData] = useState([]);
-  const [name, setQuery] = useState('');
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     let ignore = false;
 
     async function fetchData() {
-      const result = await axios(
-        `https://restcountries.eu/rest/v2/name/${name}`
-      );
+      const result = await axios(`https://restcountries.eu/rest/v2/`);
       if (!ignore) setData(result.data);
     }
 
@@ -19,12 +17,12 @@ function Countries() {
     return () => {
       ignore = true;
     };
-  }, [name]);
+  });
 
   return (
     <div style={{ textAlign: 'left', margin: '3em' }}>
       <div style={{ marginLeft: '2.5em' }}>
-        <input value={name} onChange={e => setQuery(e.target.value)} />
+        <input value={query} onChange={e => setQuery(e.target.value)} />
       </div>
       <ul style={{ listStyle: 'none' }}>
         {data.map(item => (
